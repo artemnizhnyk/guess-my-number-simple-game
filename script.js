a988bfb449ea7672892c81d5c68e6e01972128eb
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-06-04 10:58:50 +0200

    some first game logic

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const guessInput = document.querySelector('.guess');
+
+document.querySelector('.check').addEventListener('click', (ev) => {
+    const guess = +guessInput.value;
+
+    if (!guess) {
+        document.querySelector('.message').textContent = "🛑 No number";
+    }
+});
